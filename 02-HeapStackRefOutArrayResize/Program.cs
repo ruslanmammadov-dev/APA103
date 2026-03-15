89d2f65bfcac23affc0d53e492b60cd91768b125
Ruslan Memmedov <ruslanrm-apa103@code.edu.az>
2026-03-15 22:22:37 +0400

    02-HeapStackRefOutArrayResize

---
+++ 02-HeapStackRefOutArrayResize/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02_HeapStackRefOutArrayResize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3 };
+
+            ArrayResize(numbers, 4, 5, 6);
+        }
+
+        static void ArrayResize(int[] sourceArray, params int[] newElements)
+        {
+            int newLength = sourceArray.Length + newElements.Length;
+
+            int[] newArray = new int[newLength];
+
+            for (int i = 0; i < sourceArray.Length; i++)
+            {
+                newArray[i] = sourceArray[i];
+            }
+
+            for (int i = 0; i < newElements.Length; i++)
+            {
+                newArray[sourceArray.Length + i] = newElements[i];
+            }
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.Write(newArray[i] + " ");
+            }
+        }
+    }
+}
